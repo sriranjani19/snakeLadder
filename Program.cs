7e17c8fb0d86a122303f25bec0d4e415982b40c6
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-03-16 14:23:52 +0530

    program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder\n");
+
+            //StartGame.StartTheGame();
+
+            //RollingDice.DiceRoll();
+
+            Options.ChooseOptions();
+        }
+    }
+}
+
+
+
+
